d4cae6b9e37acc51ee55a29c14cb6e790bd2ea8b
= <=>
2023-07-07 22:14:03 +0200

    Initial commit

---
+++ src/main/java/com/roamrapp/userservice/RoamrUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.roamrapp.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoamrUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoamrUserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roamrapp/userservice/RoamrUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roamrapp.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamrUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
